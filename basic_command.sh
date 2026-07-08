63158ef0707310dea1a3bbdd569186e7a3bdf804
nuttawut kamchadphai <nuttawut.ka@kkumail.com>
2026-07-08 02:38:44 +0000

    add basic_command.sh

---
+++ basic_command.sh
@@ -0,0 +1,6 @@
+# add all changes to the staging area
+git add .
+# commit the changes with a message
+git commit -m "your commit message here"
+# push the changes to the remote repository
+git push origin <remote-branch-name>
